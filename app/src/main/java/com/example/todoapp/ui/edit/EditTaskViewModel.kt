aafc80021ddaa44eed0e3c388e613d246a3cdeb9
Yasuhiro Suzuki <yssz.vx2xnl@gmail.com>
2017-08-06 20:19:29 +0900

    Add ActivityForResult to the EditTaskActivity.

--- app/src/main/java/com/example/todoapp/ui/edit/EditTaskViewModel.kt
+++ app/src/main/java/com/example/todoapp/ui/edit/EditTaskViewModel.kt
@@ -3,29 +3,39 @@ package com.example.todoapp.ui.edit
 import android.arch.lifecycle.ViewModel
 import android.databinding.ObservableField
 import android.util.Log
+import com.example.todoapp.model.Task
+import com.example.todoapp.util.DateUtil
 
 /**
  * Created by Yasuhiro Suzuki on 2017/07/30.
  */
+
+typealias OnSuccess = (task: Task) -> Unit
+typealias OnError = (error: String) -> Unit
+
 class EditTaskViewModel: ViewModel() {
-
-    val title = ObservableField<String>()
-
-    val content = ObservableField<String>()
 
     val TITLE = "title"
 
     val CONTENT = "content"
 
-    fun save(onSuccess: () -> Unit, onError: (error: String) -> Unit) = when (validate()) {
+    val title = ObservableField<String>()
+
+    val content = ObservableField<String>()
+
+    fun save(onSuccess: OnSuccess, onError: OnError) = when (validate()) {
         TITLE -> onError("No title!")
         CONTENT -> onError("No content!")
-        else -> {
-            Log.d("title", title.get())
-            Log.d("content", content.get())
-            onSuccess()
-        }
+        else -> uploadTask(onSuccess, onError)
     }
+
+    private fun uploadTask(onSuccess: OnSuccess, onError: OnError) {
+        Log.d("title", title.get())
+        Log.d("content", content.get())
+        onSuccess(getTask())
+    }
+
+    fun getTask() = Task(DateUtil.timestump, DateUtil.currentDate, title.get(), content.get())
 
     fun validate() = if (!validateTitle()) TITLE else if (!validateContent()) CONTENT else ""
 
