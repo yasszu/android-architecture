b0f65d20f1c2950ad92a49bbe3abf96b2c387992
Yasuhiro Suzuki <yssz.vx2xnl@gmail.com>
2017-09-02 18:59:58 +0900

    Apply the Robolectric

--- app/src/test/java/com/example/todoapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/todoapp/ExampleUnitTest.kt
@@ -3,15 +3,23 @@ package com.example.todoapp
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@RunWith(RobolectricTestRunner::class)
+@Config(constants = BuildConfig::class)
 class ExampleUnitTest {
+
     @Test
+    @Throws(Exception::class)
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
 }
